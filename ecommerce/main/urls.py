1313e6a6b0993103745aa9a791d0e13ab9837433
ilvghst <mailme.suzan77@gmail.com>
2021-04-05 11:06:20 +0545

    update something new

--- ecommerce/main/urls.py
+++ ecommerce/main/urls.py
@@ -12,23 +12,43 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("profile/", Profile.as_view(), name="profile"),
-    path("logout-user/", LogoutCustomer.as_view(), name='logout_user'),
+    path("logout-user/", LogoutCustomer.as_view(), name="logout_user"),
     path("register/", RegisterView.as_view(), name="register"),
     path("privacy/", PrivacyView.as_view(), name="privacy"),
     path("terms/", TermsView.as_view(), name="terms"),
     path("product-wise-list/", ProductWiseListView.as_view(), name="product-wise-list"),
-	path('verification/', include('verify_email.urls')),	
-    path('activate/<str:uid>/<str:token>', Activate.as_view(), name='activate'),
-    path('reset_password/', auth_views.PasswordResetView.as_view(template_name = "main/reset_password.html"), name ='reset_password'),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name = "main/password_reset_sent.html"), name ='password_reset_done'),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name = "main/password_reset_form.html"), name ='password_reset_confirm'),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name = "main/password_reset_done.html"), name ='password_reset_complete'),
+    path("verification/", include("verify_email.urls")),
+    path("activate/<str:uid>/<str:token>", Activate.as_view(), name="activate"),
+    path(
+        "reset_password/",
+        auth_views.PasswordResetView.as_view(template_name="main/reset_password.html"),
+        name="reset_password",
+    ),
+    path(
+        "reset_password_sent/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="main/password_reset_sent.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="main/password_reset_form.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset_password_complete/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="main/password_reset_done.html"
+        ),
+        name="password_reset_complete",
+    ),
     path("product/<str:pk>/", ProductView.as_view(), name="product"),
     path("cart/", DetailCartItem.as_view(), name="cart"),
-    path("cart/delete/<pk>/",DeleteCartItem.as_view(), name="deletecartitem"),
-    path('add-to-cart/<pk>/', add_to_cart, name='add-to-cart'),
-    path("cart/update/<pk>", UpdateCartItem.as_view(), name="updatecart")
-
-
-    
+    path("cart/delete/<pk>/", DeleteCartItem.as_view(), name="deletecartitem"),
+    path("add-to-cart/<pk>/", add_to_cart, name="add-to-cart"),
+    path("cart/update/<pk>", UpdateCartItem.as_view(), name="updatecart"),
+    path("404/", PageNotFoundView.as_view(), name="404"),
 ]
